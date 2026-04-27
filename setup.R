bc0058383e6f69468a17b214447669622e710a2d
aziz483 <abdeloumarghad@gmail.com>
2026-04-27 14:00:07 +0200

    Suppression des fichiers inutiles au projet

--- setup.R
+++
@@ -1,9 +0,0 @@
-# Setup script for R project
-# Install required packages
-
-packages <- c("AER", "lmtest", "sandwich", "foreign", "MASS", "ChainLadder", "survival", "mhurdle")
-
-install.packages(packages, repos = "https://cran.rstudio.com/")
-
-# Load packages to check
-lapply(packages, library, character.only = TRUE)
